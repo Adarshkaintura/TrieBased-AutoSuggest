9c0247424577a796c20cbf8f8e7ed1a64f85b9be
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2025-11-07 18:40:31 +0530

    Update TrieNode.java

--- TrieBasedAuto/TrieNode.java
+++ TrieBasedAuto/TrieNode.java
@@ -5,5 +5,5 @@ import java.util.*;
 public class TrieNode {
     Map<Character, TrieNode> children = new HashMap<>();
     boolean isEndOfWord = false;
-    int frequency = 0; // used for ranking suggestions
+    int frequency = 0; 
 }
